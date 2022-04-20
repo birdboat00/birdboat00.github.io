6cfa11d6e430c9cf89530de06e59dd9b751367c8
birdboat00 <roidl.sebastian@outlook.com>
2022-04-20 13:36:26 +0200

    quotes

---
+++ main.js
@@ -0,0 +1,31 @@
+const rndq = document.getElementById("rndq")
+const qs = [
+  // alice in wonderland
+  "We are all mad here...",
+  "We are all mad here. I'm mad, you're mad.",
+  "Curiouser and curiouser!",
+  "How long is forever? - Sometimes, just one second.",
+  "If everybody minded their own business, the world would go round a deal faster than it does.",
+  "Everything's got a moral, if only you can find it.",
+  // alice through the looking-glass and what alice found there
+  "Consider anything, only don't cry!",
+  "Thy loving smile will surely hail The love-gift of a fairy tale.",
+  "When I use a word, it means just what I choose it to mean - neither more, nor less.",
+  "One thing was certain, that the white kitten had nothing to do with it -- it was the black kitten's failt entirely.",
+  "You've been gone too long, Alice. There are matters which might benefit from your attention. Friends cannot be neglected.",
+  "I've always wondered when soon is.",
+  // Netflix DARK
+  "Every decision for something is a decision against something else.",
+  "Most people are nothing but pawns on a chessboard led by an unknown hand.",
+  "Good and evil are a question of perspective.",
+  "The distinction between past, present and future is only a stubbornly persistent illusion.",
+  // Thirteen Reasons Why
+  "No one knows for certain how much impact they have on the lives of other people. Oftentimes, we have no clue. Yet we push it just the same.",
+  "Everything...affects everything.",
+  "You can hear rumors. But you can't know them.",
+  "After all, how often do we get a second chance?",
+  "But sometimes there's nothing left to do but move on.",
+  "It may seem like a small role nowm but it matters. In the end, everything matters.",
+  ""
+]
+rndq.innerText = qs[Math.floor(Math.random() * qs.length)]
