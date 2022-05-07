9cdfda1b96303cca9065daf9acbc9350df557f4f
birdboat00 <roidl.sebastian@outlook.com>
2022-05-07 21:11:18 +0200

    some enhancements - added unnecessary impress - loading essays from json meta file

--- essays/essay.mjs
+++ essays/essay.mjs
@@ -1,15 +1,20 @@
 import * as marked from "./marked.mjs"
 
-export function essay(meta) {
-  document.title = (meta.name ?? "untitled essay") + " | bb00"
+const getmeta = (n) => document.querySelector("meta[name=" + n)?.content;
+const essaymeta = getmeta("essay");
+if (essaymeta) {
   const content = document.getElementById("essay-content")
-
-  fetch(meta.contentfile)
-    .catch(error => {
-      content.innerText = `Failed to load essay '${meta.contentfile}'. Error: ${error}`
-    })
-    .then(res => res.text())
-    .then(text => {
-      content.innerHTML = marked.parse(text)
-    })
+  fetch(essaymeta)
+    .then(res => res.ok ? res.json() : Promise.reject(res.status))
+    .then(m => {
+      document.title = (m.name ?? "untitled essay") + " | bb00"
+      document.documentElement.setAttribute("data-theme", m.theme ?? "def")
+      fetch(m.file)
+        .then(res => res.ok ? res.text() : Promise.reject(res.status))
+        .then(t => { content.innerHTML = marked.parse(t) })
+        .catch(e => {
+          content.innerText = `Failed to load essay '${m.contentfile}'. Error: ${e}`
+        })
+     })
+    .catch(e => { content.innerText = `Failed to load essay. Error: ${e}` })
 }
