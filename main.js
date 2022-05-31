cb43139b48e245b86565676b03f17e6f14f54f30
birdboat00 <roidl.sebastian@outlook.com>
2022-05-31 21:59:24 +0200

    removed some quotes

--- main.js
+++ main.js
@@ -12,23 +12,6 @@ const qs = [
   "When I use a word, it means just what I choose it to mean - neither more, nor less.",
   "One thing was certain, that the white kitten had nothing to do with it -- it was the black kitten's failt entirely.",
   "You've been gone too long, Alice. There are matters which might benefit from your attention. Friends cannot be neglected.",
-  "I've always wondered when soon is.",
-  // Netflix DARK
-  "Every decision for something is a decision against something else.",
-  "Most people are nothing but pawns on a chessboard led by an unknown hand.",
-  "Good and evil are a question of perspective.",
-  "The distinction between past, present and future is only a stubbornly persistent illusion.",
-  // Thirteen Reasons Why
-  "No one knows for certain how much impact they have on the lives of other people. Oftentimes, we have no clue. Yet we push it just the same.",
-  "Everything...affects everything.",
-  "You can hear rumors. But you can't know them.",
-  "After all, how often do we get a second chance?",
-  "But sometimes there's nothing left to do but move on.",
-  "It may seem like a small role nowm but it matters. In the end, everything matters.",
-  // euphoria
-  "Memories exist outside of time.",
-  // midsommar
-  "Trust me. Alright?",
-
+  "I've always wondered when soon is."
 ]
 const M=Math; rq.innerText = qs[M.floor(M.random() * qs.length)]
